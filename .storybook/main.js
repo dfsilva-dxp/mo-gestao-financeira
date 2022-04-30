515b0898f7f9fc34dc06ea9eb9ab476126a6e778
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-30 12:23:40 -0300

    add boilerplate

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src")
+    ];
+
+    return config;
+  }
+};
